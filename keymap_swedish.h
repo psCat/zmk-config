e25d2c8bd6f0f9452ba03796f5af326684ebdb68
sofia <sofoxxo@gmail.com>
2025-02-04 19:55:20 +0100

    change to swedish types

--- keymap_swedish.h
+++ keymap_swedish.h
@@ -1,28 +1,49 @@
+// unused? #define SE_CURR  LS(N4)    // ¤
 
 // Done:
 #define SE_AUOO LBKT // Å
 #define SE_OUEE SEMI // Ö
 #define SE_AUEE SQT  // Ä            single quote
 
-#define SE_EXCL  LS(N1)    // !
-#define SE_DQT   LS(N2)    // "
-#define SE_HASH  LS(N3)    // #
-// unused? #define SE_CURR  LS(N4)    // ¤
-#define SE_PRCNT LS(N5)    // %
-#define SE_AMPS  LS(N6)    // &
-#define SE_SLASH LS(N7)    // /
-#define SE_LPAR  LS(N8)    // (
-#define SE_RPAR  LS(N9)    // )
-#define SE_EQUAL LS(N0)    // =
+#define SE_EXCL  LS(N1)     // !
+#define SE_DQT   LS(N2)     // "
+#define SE_HASH  LS(N3)     // #
+#define SE_PRCNT LS(N5)     // %
+#define SE_AMPS  LS(N6)     // &
+#define SE_SLASH LS(N7)     // /
+#define SE_LPAR  LS(N8)     // (
+#define SE_RPAR  LS(N9)     // )
+#define SE_LBRC RA(SE_7)    // {
+#define SE_RBRC RA(SE_0)    // }
+#define SE_LBKT RA(SE_8)    // [
+#define SE_RBKT RA(SE_9)    // ]
+#define SE_EQUAL LS(N0)     // =
+#define SE_AT    RA(N2)     // @
+#define SE_SEMI LS(SE_COMM) // ;
+#define SE_PIPE RA(LT)      // |
+#define SE_EQUAL LS(N0)     // =
+#define SE_QMARK LS(PLUS)   // ?
+#define SE_LT NUBS          // <
+#define SE_GT LS(SE_LT)     // >
+#define SE_COLON LS(DOT)    // :
+#define SE_CARET LS(RBKT)   // ^ (dead)
+#define SE_GRAVE LS(EQUAL)  // ` (dead)
+#define SE_STAR LS(SE_APOS) // *
+#define SE_MINUS SLASH      // -
+#define SE_DOLLAR  RA(N4)   // $
+#define SE_BSLH EQUAL       // ´ (dead)
+#define SE_PLUS MINS       // +
+#define SE_APOS NUHS       // '
+#define SE_TILDE2 TILDE2
+// TILDE2
+
+
+// #define SE_UNDS S(SE_MINS) // _
+// #define SE_ACUT KC_EQL  // ´ (dead)
 
 // Aliases
 // #define SE_SECT KC_GRV  // §
-// #define SE_PLUS KC_MINS // +
-// #define SE_ACUT KC_EQL  // ´ (dead)
-// #define SE_DIAE KC_RBRC // ¨ (dead)
-// #define SE_QUOT KC_NUHS // '
-// #define SE_LABK KC_NUBS // <
-// #define SE_MINS KC_SLSH // -
+// #define SE_DIAE  // ¨ (dead)
 // #define SE_HALF S(SE_SECT) // ½
 // #define SE_EXLM S(SE_1)    // !
 // #define SE_DQUO S(SE_2)    // "
@@ -33,26 +54,10 @@
 // #define SE_SLSH S(SE_7)    // /
 // #define SE_LPRN S(SE_8)    // (
 // #define SE_RPRN S(SE_9)    // )
-// #define SE_EQL  S(SE_0)    // =
-// #define SE_QUES S(SE_PLUS) // ?
-// #define SE_GRV  S(SE_ACUT) // ` (dead)
-// #define SE_CIRC S(SE_DIAE) // ^ (dead)
-// #define SE_ASTR S(SE_QUOT) // *
-// #define SE_RABK S(SE_LABK) // >
-// #define SE_SCLN S(SE_COMM) // ;
-// #define SE_COLN S(SE_DOT)  // :
-// #define SE_UNDS S(SE_MINS) // _
-// #define SE_AT   ALGR(SE_2)    // @
 // #define SE_PND  ALGR(SE_3)    // £
-// #define SE_DLR  ALGR(SE_4)    // $
 // #define SE_EURO ALGR(SE_5)    // €
-// #define SE_LCBR ALGR(SE_7)    // {
-// #define SE_LBRC ALGR(SE_8)    // [
-// #define SE_RBRC ALGR(SE_9)    // ]
-// #define SE_RCBR ALGR(SE_0)    // }
 // #define SE_BSLS ALGR(SE_PLUS) // (backslash)
 // #define SE_TILD ALGR(SE_DIAE) // ~ (dead)
-// #define SE_PIPE ALGR(SE_LABK) // |
 // #define SE_MICR ALGR(SE_M)    // µ
 
 
